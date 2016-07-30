4fef767b0a21fc1f763386fb55cc6f1195241fb6
peperontino39 <peperrontino39@docomo.ne.jp>
2016-07-30 19:17:52 +0900

    タイトルでエネミーの

--- src/Scene/SceneCategory/Title.cpp
+++ src/Scene/SceneCategory/Title.cpp
@@ -6,7 +6,7 @@
 #include "../../Object/GameObject/Camera/MainCamera.h"
 #include "../../Object/GameObject/SignPost/SignPostManager.h"
 #include "../../Object/GameObject/CharaBase/Player/Player.h"
-
+#include "../../Object/GameObject/CharaBase/Enemy/Enemy.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -21,6 +21,8 @@ void Title::setup()
 	entities.setObject<ar::SignPostManager>();
 	entities.setObject<ar::SampleObject>();
 	entities.setObject<Player>();
+	entities.setObject<Enemy>();
+	entities.getObject<Enemy>()->setSignPostManager(entities.getObject<ar::SignPostManager>());
 
 	entities.setupGameObject();
 
