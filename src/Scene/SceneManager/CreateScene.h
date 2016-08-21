8ae4861dba2c6213d3071ec0ee90e562e4244a9f
しゃき〜ん <Lacty@users.noreply.github.com>
2016-08-21 20:14:38 +0900

    add comment

--- src/Scene/SceneManager/CreateScene.h
+++ src/Scene/SceneManager/CreateScene.h
@@ -5,6 +5,9 @@
 class Scene
 {
 public:
+        // templateはなんでも受け付けてしまうので注意
+        // SceneBaceのポインタを仮引数に設定してあげれば
+        // シーンのみ受け付ける関数が作成できる
 	template<typename T>
 	static void createScene(T* _scene) {
 		scene = std::make_shared<T>(*_scene);
@@ -15,5 +18,6 @@ public:
 	}
 
 private:
+        // unique_ptrを検討しよう
 	static std::shared_ptr<SceneBase> scene;
 };
